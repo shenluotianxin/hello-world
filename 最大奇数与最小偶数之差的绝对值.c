097f634b8af87a505abe300fc62732f751515a63
shenluotianxin <1154761786@qq.com>
2016-04-08 19:47:25 +0800

    更新c程作业

--- 最大奇数与最小偶数之差的绝对值.c
+++ 最大奇数与最小偶数之差的绝对值.c
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 int main(){
  int a[6];
@@ -6,18 +7,20 @@ int main(){
  {
  	cin>>a[i];
 	}
- int l=0,r=6;
- while (l<=r){
-  bool left=a[l]%2==1;
-  bool right=a[r]%2==0;
-  if(left){
-  l++;
-  }else if(right){
-  r--;
-  }else if(!left&&!right){
-  int temp =a[l];
-  a[l]=a[r];
-  a[r]=temp;
-  }
+ int b,c,max=0,min=100;
+ for(int i=0;i<6;i++)
+ {
+ 	if (a[i]%2!=0)
+		{b=a[i];
+		 if(b>max)
+		 	max=b;
+		}
+	else
+		{c=a[i];
+		if(c<min)
+			min=c;
+			}
  }
- 
+ cout<<abs(max-min)<<endl;
+ return 0;
+ }
